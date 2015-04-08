066abe02142dcb6f1b2c3efcf6d90b864db6319b
Barton Phillips <barton@bartonphillips.com>
2015-04-08 16:01:42 -0600

    added pdf files

--- mk-html.sh
+++ mk-html.sh
@@ -3,6 +3,8 @@
 
 # Make .html files from .md files
 
-pandoc -fmarkdown_github README.md > README.html
-pandoc -fmarkdown_github examples/EXAMPLES.md > examples/EXAMPLES.html
+pandoc -fmarkdown_github README.md -o README.html
+pandoc -fmarkdown_github README.md -o README.pdf 
+pandoc -fmarkdown_github examples/EXAMPLES.md -o examples/EXAMPLES.html
+pandoc -fmarkdown_github examples/EXAMPLES.md -o examples/EXAMPLES.pdf
 
