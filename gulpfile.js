e008bcf14f343865fc2dee6c69c8a3625198d019
Barton Phillips <barton@bartonphillips.com>
2015-04-16 13:54:15 -0600

    Add EXAMPLE

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var readmeHeader = "<!DOCTYPE html>\n<html>\n<head>\n<style>\npre {\n"+
                    "background-color: #DBDBDB;\n"+
                    "overflow: auto;\n}\n</style>\n</head>\n<body>\n";
 
-gulp.task('default', function() {
+gulp.task('readmehtml', function() {
   return gulp.src("README.md")
       .pipe(exec('pandoc -f markdown_github <%= file.path %> -t html',
                  {pipeStdout: true}))
@@ -22,8 +22,21 @@ gulp.task('default', function() {
       .pipe(gulp.dest('./'))
 });
 
+gulp.task('exampleshtml', function() {
+  return gulp.src("examples/EXAMPLES.md")
+      .pipe(exec('pandoc -f markdown_github <%= file.path %> -t html',
+                 {pipeStdout: true}))
+      .pipe(header(readmeHeader))
+      .pipe(footer("\n</body>\n</html>\n"))
+      .pipe(rename('EXAMPLES.html'))
+      .pipe(gulp.dest('./examples/'))
+});
+
+gulp.task('default', ['readmehtml', 'exampleshtml']);
+
 // Watch for changes to README.md
 
 gulp.task('watch', function() {
-  gulp.watch('README.md', ['default']);
+  gulp.watch('README.md', ['readmehtml']);
+  gulp.watch('examples/EXAMPLES.md', ['exampleshtml']);  
 });
