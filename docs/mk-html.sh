1e0039e78517eecdbb5754085e862c7a21879771
Barton Phillips <bartonphillips@gmail.com>
2022-09-06 17:51:54 -0400

    Added examplereadme. 	new file:   docs/examplereadme.html 	modified:   docs/index.html 	modified:   docs/mk-html.sh

--- docs/mk-html.sh
+++ docs/mk-html.sh
@@ -23,4 +23,6 @@ pagetitle="Additional Files";
 /usr/bin/pandoc -f gfm -t html5 -Vpagetitle="$pagetitle" -Vmath="$css" -s files.md -o files.html
 pagetitle="Analysis";
 /usr/bin/pandoc -f gfm -t html5 -Vpagetitle="$pagetitle" -Vmath="$css" -s analysis.md -o analysis.html
+pagetitle="examplereadme";
+/usr/bin/pandoc -f gfm -t html5 -Vpagetitle="$pagetitle" -Vmath="$css" -s examplereadme.md -o examplereadme.html
 
