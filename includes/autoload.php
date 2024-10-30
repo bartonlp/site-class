b0b2ea779ea002368af80b0dc9fdff7ffbcbe85f
Barton Phillips <bartonphillips@gmail.com>
2024-10-30 16:20:37 +0000

    Deleted aboutwebsite.php, examples/aboutwebsite.php and includes/mysitemap.json. I don't believe they are used anywhere. Updated includes/autoload.php. Removed <<<< from pull. 	deleted:    aboutwebsite.php 	deleted:    examples/aboutwebsite.php 	modified:   includes/autoload.php (added findmysitemap() to replace just getting it form this dir.) 	modified:   includes/database-engines/dbPdo.class.php (removed <<< from pull) 	deleted:    includes/mysitemap.json

--- includes/autoload.php
+++ includes/autoload.php
@@ -5,7 +5,7 @@ namespace bartonlp\siteload;
 
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_WARNING & ~E_NOTICE);
 
-define("SITELOAD_VERSION", "1.1.3autoload-pdo");
+define("SITELOAD_VERSION", "1.1.4autoload-pdo"); // BLP 2024-10-30 - modify, add findmysitemap().
 define("SITECLASS_DIR", __DIR__);
 
 function getSiteloadVersion() {
@@ -35,6 +35,11 @@ date_default_timezone_set('America/New_York'); // Done here and in dbPdo.class.p
 
 if($_SERVER['HTTP_HOST'] == "195.252.232.86") return; 
 
+//vardump("server", $_SERVER);
+
+$mydir = dirname($_SERVER['SCRIPT_FILENAME']);
+//echo "script_filename: $mydir<br>";
+
 if($__VERSION_ONLY) {
   return SITELOAD_VERSION;
 } else {
@@ -42,6 +47,40 @@ if($__VERSION_ONLY) {
     if(file_exists("../bartonphillips.org:8000")) $port = ":8000";
     return json_decode(stripComments(file_get_contents("https://bartonphillips.org$port/mysitemap.json")));
   } else {
-    return json_decode(stripComments(file_get_contents(__DIR__ ."/mysitemap.json")));
+    return findsitemap(); // BLP 2024-10-30 - use ne findsitmap() function borowed from siteload.php with modifications
+  }
+}
+
+// Find the mysitemap.json. $mydir is a global. This is borrowed from siteload.php with
+// modification.
+
+function findsitemap() {
+  global $mydir;
+
+  if(file_exists($mydir . "/mysitemap.json")) {
+    // BLP 2023-08-17 - use the stripComments() from the helperfunctions.php
+
+    return json_decode(stripComments(file_get_contents($mydir . "/mysitemap.json")));
+  } else {
+    // If we didn't find the mysitemap.json then have we reached to docroot? Or have we reached the
+    // root. We should actually never reach the root.
+
+    if(($_SERVER['DOCUMENT_ROOT'] ?? $S_SERVER['VIRTUALHOST_DOCUMENT_ROOT']) == $mydir || '/' == $mydir) {
+      echo <<<EOF
+<h1>NO 'mysitemap.json' Found</h1>
+<p>To run {$_SERVER['PHP_SELF']} you must have a 'mysitemap.json' somewhere within the Document Root.</p>
+EOF;
+      error_log("ERROR: siteload.php. No 'mysitemap.json' found in " . getcwd() . " for file {$_SERVER['PHP_SELF']}. DocRoot: $docroot");
+      exit();
+    }
+
+    // We are not at the root so do $mydir = dirname($mydir). For example if $mydir is
+
+    $mydir = dirname($mydir);
+    //echo "mydir: $mydir<br>";
+
+    // Recurse
+
+    return findsitemap();
   }
 }
