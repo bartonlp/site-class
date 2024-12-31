f3a8d14ac22629bd0773c45fe176896234c60595
Barton Phillips <bartonphillips@gmail.com>
2024-12-31 20:11:49 +0000

    modified:   update.sh

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ git add .
 git commit
 git log --all --graph -p --decorate > ~/www/bartonlp.com/gitlog
 git log --all --graph -p --decorate -2 > ./limited-gitlog
-echo "Run 'git push' when ready.
+echo "Run 'git push' when ready".
