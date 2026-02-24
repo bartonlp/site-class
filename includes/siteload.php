e576321adf7d4c3e4837a11307d4324feea1e293
Barton Phillips <bartonphillips@gmail.com>
2026-02-24 07:09:46 -0500

    Stupid I did audoload not autoload. 	modified:   includes/siteload.php

--- includes/siteload.php
+++ includes/siteload.php
@@ -1,7 +1,4 @@
 <?php
-$newdir = __DIR__;
-var_dump($newdir);
 $mysiteload = true;
 require __DIR__."/../../../autoload.php";
-
-return require __DIR__ . "/includes/audoload.php";
+return require __DIR__ . "/includes/autoload.php";
